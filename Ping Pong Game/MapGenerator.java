c6b00fc64a15ec27935ee46735b6b98f0597b4e4
kasulasaitejachary <98974519+kasulasaitejachary@users.noreply.github.com>
2023-05-30 18:15:47 +0530

    Update MapGenerator.java

--- Ping Pong Game/MapGenerator.java
+++ Ping Pong Game/MapGenerator.java
@@ -12,9 +12,9 @@ public class MapGenerator {
     public int map[][];
     public int bricksWidth;
     public int bricksHeight;
-    public MapGenerator(int row , int col){
+    public MapGenerator(int row , int col){ // this create  the brick of size 3x7.
         map = new int[row][col];
-        for (int[] map1 : map) {
+         for (int i=0; i<map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
                 map1[j] = 1;
             }
@@ -22,7 +22,7 @@ public class MapGenerator {
         bricksWidth = 540/col;
         bricksHeight = 150/row;
     }
-    public void draw(Graphics2D g) {
+    public void draw(Graphics2D g) { // This draws brick
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
                 if (map[i][j] > 0) {
@@ -38,7 +38,7 @@ public class MapGenerator {
 
         }
     }
-    public void setBricksValue(int value,int row,int col)//to change value from 1 to 0(ball hits brick)
+    public void setBricksValue(int value,int row,int col)//to change value from 1 to 0(ball hits brick) //to change value from 1 to 0(ball hits brick)
     {
         map[row][col] = value;
 
